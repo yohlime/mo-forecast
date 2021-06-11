2ab2d9349fcddfdff01bd16d210f3943deccbe79
Emilio Gozo <egozo@observatory.ph>
2021-06-11 14:44:35 +0800

    chore: use Intel MPI

--- scripts/set_env_wrf.run.sh
+++ scripts/set_env_wrf.run.sh
@@ -1,10 +1,18 @@
 module purge
-module load intel
+module use /opt/intel/oneapi/modulefiles
+module load mpi compiler
+
+export I_MPI_PMI_LIBRARY=/usr/lib64/libpmi.so
+export I_MPI_CC=icc
+export I_MPI_CXX=icpc
+export I_MPI_FC=ifort
+export I_MPI_F77=ifort
+export I_MPI_F90=ifort
 
 spack env deactivate
-spack env activate parallel-openmpi-slurm
+spack env activate intel
 spack load netcdf-fortran %intel
 spack load parallel-netcdf %intel
+spack load jasper %intel
 
-export WRFIO_NCD_LARGE_FILE_SUPPORT=1
 ulimit -s unlimited
