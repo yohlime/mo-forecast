b4ceb2bd669eb4ebc125a44d1cfb156ecfa75d0f
Emilio Gozo <egozo@observatory.ph>
2021-10-18 11:49:30 +0800

    feat: add --no-{download, upload, post-proc} options

--- scripts/run_fcst.sh
+++ scripts/run_fcst.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+DOWNLOAD_INPUT=true
+UPLOAD_OUTPUT=true
+POST_PROCESS=true
+
+while [ True ]; do
+if [ "$1" = "--no-download"]; then
+    DOWNLOAD_INPUT=false
+    shift 1
+elif [ "$1" = "--no-upload"]; then
+    UPLOAD_OUTPUT=false
+    shift 1
+elif [ "$1" = "--no-post-proc"]; then
+    POST_PROCESS=false
+    shift 1
+else
+    break
+fi
+done
+
 echo "------------------"
 echo " Starting WRF Forecast "
 echo "------------------"
@@ -8,8 +27,10 @@ source $SCRIPT_DIR/set_date_vars.sh
 
 mkdir -p $GFSDIR
 
-# download GFS
-source $SCRIPT_DIR/download_gfs.aria2.sh
+if [ "$DOWNLOAD_INPUT" = true ]; then
+  # download GFS
+  source $SCRIPT_DIR/download_gfs.aria2.sh
+fi
 
 NDL_FILES=$(ls $GFSDIR/*.grb | wc -l)
 
@@ -36,34 +57,38 @@ if [ $NDL_FILES -eq $NUM_FILES ]; then
     done
   done <<< "$WRF_RUN_NAMES"
 
-  # Post processing
+  if [ "$POST_PROCESS" = true ]; then
+    # Post processing
 
-  ### Python Post processing
-  slurm_opts="-A $SLURM_ACCOUNT -p $SLURM_PARTITION -N $SLURM_NUM_NODES -d afterok:$prev_jid -J python-$FCST_YYYYMMDD$FCST_ZZ -o ${OUTDIR}/logs/python/python_$FCST_YYYYMMDD$FCST_ZZ.out -n 12"
-  prev_jid=$(sbatch $slurm_opts $SCRIPT_DIR/postproc_python.sh)
-  prev_jid=$(echo $prev_jid | tr -dc '0-9')
+    ### Python Post processing
+    slurm_opts="-A $SLURM_ACCOUNT -p $SLURM_PARTITION -N $SLURM_NUM_NODES -d afterok:$prev_jid -J python-$FCST_YYYYMMDD$FCST_ZZ -o ${OUTDIR}/logs/python/python_$FCST_YYYYMMDD$FCST_ZZ.out -n 12"
+    prev_jid=$(sbatch $slurm_opts $SCRIPT_DIR/postproc_python.sh)
+    prev_jid=$(echo $prev_jid | tr -dc '0-9')
 
-  ### Web Upload
-  slurm_opts="-A $SLURM_ACCOUNT -p $SLURM_PARTITION -N $SLURM_NUM_NODES -d afterok:$prev_jid -J web-upload-$FCST_YYYYMMDD$FCST_ZZ -o $MAINDIR/output/web-upload_$FCST_YYYYMMDD$FCST_ZZ.log -n 1"
-  sbatch $slurm_opts $SCRIPT_DIR/web_upload.sh
+    if [ "$UPLOAD_OUTPUT" = true ]; then
+      ### Web Upload
+      slurm_opts="-A $SLURM_ACCOUNT -p $SLURM_PARTITION -N $SLURM_NUM_NODES -d afterok:$prev_jid -J web-upload-$FCST_YYYYMMDD$FCST_ZZ -o $MAINDIR/output/web-upload_$FCST_YYYYMMDD$FCST_ZZ.log -n 1"
+      sbatch $slurm_opts $SCRIPT_DIR/web_upload.sh
+    fi
 
-  ### ARWpost
-  ### schedule multiple slurm ARWpost jobs based on WRF_RUN_NAMES
-  run_idx=1
-  while IFS=':' read -ra RUN_NAMES; do
-    for run_name in "${RUN_NAMES[@]}"; do
-      export NAMELIST_RUN=$run_name
+    ### ARWpost
+    ### schedule multiple slurm ARWpost jobs based on WRF_RUN_NAMES
+    run_idx=1
+    while IFS=':' read -ra RUN_NAMES; do
+      for run_name in "${RUN_NAMES[@]}"; do
+        export NAMELIST_RUN=$run_name
 
-      slurm_opts="-A $SLURM_ACCOUNT -p $SLURM_PARTITION -N $SLURM_NUM_NODES -d afterok:$prev_jid -J arw-$FCST_YYYYMMDD${FCST_ZZ}_run${run_idx} -o $WRF_MAINDIR/ARWpost/arw_$FCST_YYYYMMDD${FCST_ZZ}_run${run_idx}.out -n 1"
-      prev_jid=$(sbatch $slurm_opts $SCRIPT_DIR/run_arwpost.sh)
-      prev_jid=$(echo $prev_jid | tr -dc '0-9')
+        slurm_opts="-A $SLURM_ACCOUNT -p $SLURM_PARTITION -N $SLURM_NUM_NODES -d afterok:$prev_jid -J arw-$FCST_YYYYMMDD${FCST_ZZ}_run${run_idx} -o $WRF_MAINDIR/ARWpost/arw_$FCST_YYYYMMDD${FCST_ZZ}_run${run_idx}.out -n 1"
+        prev_jid=$(sbatch $slurm_opts $SCRIPT_DIR/run_arwpost.sh)
+        prev_jid=$(echo $prev_jid | tr -dc '0-9')
 
-      run_idx=$((run_idx+1))
-    done
-  done <<< "$WRF_RUN_NAMES"
+        run_idx=$((run_idx+1))
+      done
+    done <<< "$WRF_RUN_NAMES"
 
-  ### ENSEMBLE Post processing
-  ens_slurm_opts="-A $SLURM_ACCOUNT -p $SLURM_PARTITION -N $SLURM_NUM_NODES -d afterok:$prev_jid -J ens-$FCST_YYYYMMDD$FCST_ZZ -o $WRF_MAINDIR/ENSEMBLE/ens_$FCST_YYYYMMDD$FCST_ZZ.out -n 1"
-  sbatch $ens_slurm_opts $SCRIPT_DIR/run_post_ens.sh
+    ### ENSEMBLE Post processing
+    ens_slurm_opts="-A $SLURM_ACCOUNT -p $SLURM_PARTITION -N $SLURM_NUM_NODES -d afterok:$prev_jid -J ens-$FCST_YYYYMMDD$FCST_ZZ -o $WRF_MAINDIR/ENSEMBLE/ens_$FCST_YYYYMMDD$FCST_ZZ.out -n 1"
+    sbatch $ens_slurm_opts $SCRIPT_DIR/run_post_ens.sh
+  fi
 
 fi
