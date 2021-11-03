f83519ea60f92e20c2c173fc6301ebda6c74ac1a
Emilio Gozo <egozo@observatory.ph>
2021-11-03 11:53:35 +0800

    fix: temp/hi y-axis tick labels

--- scripts/python/plot_ts.py
+++ scripts/python/plot_ts.py
@@ -133,7 +133,6 @@ def plot_timeseries(ds, out_dir):
     axs[1].set_xlabel(None)
     axs[1].set_xlim([site_df.index.min() - timedelta(hours=2), site_df.index.max()])
 
-    axs[1].set_yticks(range(25, 45, 5))
     axs[1].tick_params(axis="y", labelsize=24)
     axs[1].set_ylabel("Temp/HI (°C)", fontsize=24, color="red")
     y_min = site_df[["temp", "hi"]].min().min()
@@ -141,6 +140,7 @@ def plot_timeseries(ds, out_dir):
     y_max = site_df[["temp", "hi"]].max().max()
     y_max = np.ceil(y_max / 5.0) * 5
     axs[1].set_ylim([y_min, y_max])
+    axs[1].set_yticks(range(int(y_min), int(y_max) + 1, 5))
 
     axs[1].grid(which="major", c="gray", ls="dotted")
 
