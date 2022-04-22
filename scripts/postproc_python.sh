cdb2ed1fc778159de7612aa717a13410429453cb
Emilio Gozo <egozo@observatory.ph>
2022-04-22 10:31:08 +0800

    refactor: move python related env to main script

--- scripts/postproc_python.sh
+++ scripts/postproc_python.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
 
-source $SCRIPT_DIR/set_env_wrf.run.sh
-PYTHONCONDA=/home/miniconda3/envs/toolbox/bin/python
-PYTHONPATH=$SCRIPT_DIR/python:${PYTHONPATH}
-MPLBACKEND="agg"
-
-WRF_OUT_FILE="wrfout_d01_${FCST_YY}-${FCST_MM}-${FCST_DD}_${FCST_ZZ}_00_00_5-day_fcst_rain"
-
+source ${SCRIPT_DIR}/set_env_wrf.run.sh
 
 echo "---------------------------------"
 echo " Starting python postprocessing! "
 echo "---------------------------------"
 
-
 # -------------------------------------------- #
 #              Postprocessing                  #
 # -------------------------------------------- #
 
-cd $SCRIPT_DIR/python
-$PYTHONCONDA process_wrf.py -i ${WRF_OUT_FILE} -o ${OUTDIR}
+cd ${SCRIPT_DIR}/python
+WRF_OUT_FILE="wrfout_d01_${FCST_YY}-${FCST_MM}-${FCST_DD}_${FCST_ZZ}_00_00_5-day_fcst_rain"
+${PYTHON} process_wrf.py -i ${WRF_OUT_FILE} -o ${OUTDIR}
 
 echo "---------------------------------"
 echo " Python postprocessing finished! "
