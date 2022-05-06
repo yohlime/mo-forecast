02d7aabc20e3224985dc20793f9263aa624d3970
Emilio Gozo <egozo@observatory.ph>
2022-05-06 08:42:33 +0800

    feat: upload timestamped files

--- scripts/web_upload.sh
+++ scripts/web_upload.sh
@@ -1,43 +1,40 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 YY=${FCST_YYYYMMDD:0:4}
 mm=${FCST_YYYYMMDD:4:2}
 dd=${FCST_YYYYMMDD:6:2}
 
-DATE2=$(date -d "$YY-$mm-${dd} $FCST_ZZ:00:00 8 hours" +'%Y-%m-%d %H:%M:%S')
-read YY2 mm2 dd2 HH2 MM2 SS2 <<< ${DATE2//[-: ]/ }
+DATE2=$(date -d "$YY-$mm-$dd $FCST_ZZ:00:00 8 hours" +'%Y-%m-%d %H:%M:%S')
+read -r YY2 mm2 dd2 HH2 MM2 SS2 <<<"${DATE2//[-: ]/ }"
 
 echo "--------------------------"
 echo " Uploading files for web  "
 echo "--------------------------"
 
-# SRCDIR=${OUTDIR}/.test
 SRCDIR=${OUTDIR}
 
-cat << EOF > ${SRCDIR}/info.json
+cat <<EOF >"$SRCDIR/info.json"
 {
-    "year": "${YY2}",
-    "month": "${mm2}", 
-    "day": "${dd2}", 
-    "hour": "${HH2}"
+    "year": "$YY2",
+    "month": "$mm2", 
+    "day": "$dd2", 
+    "hour": "$HH2"
 }
 EOF
-scp ${SRCDIR}/info.json panahon.linode:~/websites/panahon/resources/model/
-scp ${SRCDIR}/info.json panahon.alapaap:~/websites/panahon-php/resources/model/
+scp "$SRCDIR/info.json" panahon.linode:~/websites/panahon/resources/model/
+scp "$SRCDIR/info.json" panahon.alapaap:~/websites/panahon-php/resources/model/
 
-scp ${SRCDIR}/maps/wrf-*$YY2-$mm2-${dd2}_${HH2}PHT.png panahon.linode:~/websites/panahon/resources/model/img/
-scp ${SRCDIR}/maps/wrf-*$YY2-$mm2-${dd2}_${HH2}PHT.png panahon.alapaap:~/websites/panahon-php/resources/model/img/
+# panahon.observatory.ph
+scp "$SRCDIR/maps"/wrf-*"$YY2-$mm2-${dd2}_${HH2}PHT.png" panahon.linode:~/websites/panahon/resources/model/img/
+scp "$SRCDIR/maps"/wrf-*"$YY2-$mm2-${dd2}_${HH2}PHT.png" panahon.alapaap:~/websites/panahon-php/resources/model/img/
+scp "$SRCDIR/timeseries/img"/wrf-*"$YY2-$mm2-${dd2}_${HH2}PHT.png" panahon.linode:~/websites/panahon/resources/model/img/
+scp "$SRCDIR/timeseries/img"/wrf-*"$YY2-$mm2-${dd2}_${HH2}PHT.png" panahon.alapaap:~/websites/panahon-php/resources/model/img/
 
-scp ${SRCDIR}/timeseries/img/wrf-*$YY2-$mm2-${dd2}_${HH2}PHT.png panahon.linode:~/websites/panahon/resources/model/img/
-scp ${SRCDIR}/timeseries/img/wrf-*$YY2-$mm2-${dd2}_${HH2}PHT.png panahon.alapaap:~/websites/panahon-php/resources/model/img/
-
-for f in $(find ${SRCDIR}/web/maps/ -type f -name "wrf-*$YY2-$mm2-${dd2}_${HH2}PHT.png"); do cp "$f" ${f%_$YY2-$mm2-${dd2}_${HH2}PHT.png}_latest.png; done
-scp ${SRCDIR}/web/maps/*_latest.png panahon.linode:~/websites/panahon/resources/model/web_img/
-scp ${SRCDIR}/web/maps/*_latest.png panahon.alapaap:~/websites/panahon-php/resources/model/web_img/
-
-for f in $(find ${SRCDIR}/web/json/ -type f -name "forecast_*$YY2-$mm2-${dd2}_${HH2}PHT.json"); do cp "$f" ${f%_$YY2-$mm2-${dd2}_${HH2}PHT.json}_latest.json; done
-scp ${SRCDIR}/web/json/forecast_latest.json panahon.linode:~/websites/panahon/resources/model/
-scp ${SRCDIR}/web/json/forecast_latest.json panahon.alapaap:~/websites/panahon-php/resources/model/
+# panahon.observatory.ph/ecw
+scp "$SRCDIR/web/maps"/wrf-*"$YY2-$mm2-${dd2}_${HH2}PHT.png" panahon.linode:~/websites/panahon/resources/model/web_img/
+scp "$SRCDIR/web/maps"/wrf-*"$YY2-$mm2-${dd2}_${HH2}PHT.png" panahon.alapaap:~/websites/panahon-php/resources/model/web_img/
+scp "$SRCDIR/web/json/forecast_$YY2-$mm2-${dd2}_${HH2}PHT.json" panahon.linode:~/websites/panahon/resources/model/
+scp "$SRCDIR/web/json/forecast_$YY2-$mm2-${dd2}_${HH2}PHT.json" panahon.alapaap:~/websites/panahon-php/resources/model/
 
 echo "-----------------------------"
 echo " Done uploading web files!!! "
@@ -47,9 +44,9 @@ echo "--------------------------"
 echo " Uploading files for ACEN  "
 echo "--------------------------"
 
-rclone copy ${SRCDIR}/acenergy/csv/acenergy_SACASOL_${YY2}-${mm2}-${dd2}_${HH2}PHT.csv ecw_acen:/ACEN/csv/
-rclone copy ${SRCDIR}/acenergy/csv/acenergy_SOLARACE1_${YY2}-${mm2}-${dd2}_${HH2}PHT.csv ecw_acen:/ACEN/csv/
-rclone copy ${SRCDIR}/acenergy/img/acenergy-ts_${YY2}-${mm2}-${dd2}_${HH2}PHT.png ecw_acen:/ACEN/img/
+rclone copy "$SRCDIR/acenergy/csv/acenergy_SACASOL_$YY2-$mm2-${dd2}_${HH2}PHT.csv" ecw_acen:/ACEN/csv/
+rclone copy "$SRCDIR/acenergy/csv/acenergy_SOLARACE1_$YY2-$mm2-${dd2}_${HH2}PHT.csv" ecw_acen:/ACEN/csv/
+rclone copy "$SRCDIR/acenergy/img/acenergy-ts_$YY2-$mm2-${dd2}_${HH2}PHT.png" ecw_acen:/ACEN/img/
 
 echo "-----------------------------"
 echo " Done uploading ACEN files!!! "
