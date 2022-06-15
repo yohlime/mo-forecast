bbcefadcdf0761f4497839149916d1bc66d561ac
Emilio Gozo <egozo@observatory.ph>
2022-06-15 16:42:53 +0800

    chore: define dynamic variables on seperate files

--- scripts/MO2LITTLER/prepare_stations.py
+++ scripts/MO2LITTLER/prepare_stations.py
@@ -1,60 +1,70 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# convert SM mall station data to readable data for convert.exe
-# 34 Stations in masterlist "coordinates.csv" as of May 2022
-
-IN_DIR  = '/home/modelman/forecast/input/aws_files'
-# WORK_DIR = '/home/modelman/forecast/model/MO2LITTLER'
-OUT_DIR = '/home/modelman/forecast/input/aws_files/MO2LITTLER/point'
-yyyymmdd = '2022-05-12'
-init = '01PHT'
+from dotenv import dotenv_values
 from pathlib import Path
 import pandas as pd
 import numpy as np
-from dateutil import tz
-from datetime import datetime,tzinfo
-import pytz
 
-df_station = pd.read_csv(Path(IN_DIR) / f'coordinates.csv',delimiter='\t')
-stations = df_station['Station ID'].tolist()
-j= 1
+config = dotenv_values("./.env")
+
+df_station = pd.read_csv(Path(config["IN_DIR"]) / "coordinates.csv", delimiter="\t")
+stations = df_station["Station ID"].tolist()
+j = 1
 stationlist = []
 for i in stations:
     print(i)
-# i='SM_Marikina'
-    raw = pd.read_csv(Path(IN_DIR)/ f'stn_obs_24hr_{yyyymmdd}_{init}.csv')
-    raw['name'] = raw['name'].str.replace(' ','_')
-    raw['name'] = raw['name'].str.replace('.','')
-    raw = raw.loc[raw['name'] == f'{i}']
-    if len(raw) == 0: # if there are no station values
+    # i='SM_Marikina'
+    raw = pd.read_csv(
+        Path(config["IN_DIR"])
+        / f"stn_obs_24hr_{config['YYYYMMDD']}_{config['HH_INIT']}.csv"
+    )
+    raw["name"] = raw["name"].str.replace(" ", "_")
+    raw["name"] = raw["name"].str.replace(".", "")
+    raw = raw.loc[raw["name"] == f"{i}"]
+    if len(raw) == 0:  # if there are no station values
         continue
-    raw['date'] = pd.to_datetime(raw['timestamp'])
-    utc = raw['date'].dt.tz_convert('Etc/GMT+1')
-    raw['Stn.No.'] = j
-    raw['Year']    = utc.dt.year
-    raw['Month']   = utc.dt.month
-    raw['Day']     = utc.dt.day
-    raw['HH']      = utc.dt.hour
-    raw['T']       = raw['temp']
-    raw['W']       = raw['wspd']
-    raw['WD']      = raw['wdir']
-    raw['u']       = -raw['W']*np.cos(2*np.pi/360*(90-raw['WD']))
-    raw['v']       = -raw['W']*np.sin(2*np.pi/360*(90-raw['WD']))
-    raw['p']       = raw['pres']
-    raw['R']       = raw['rr']
-    raw['RH']      = raw['rh']
-    raw['is_sound'] = '.false.'
-    clean = raw[['Stn.No.','Year','Month','Day','HH',
-                        'T','W','WD','u','v','p','R','RH','is_sound']]
-    cleans = clean.sort_values(by=['Day','HH'])
-    cleans.to_csv(Path(OUT_DIR) /f'{i}.csv',index=False)
-    j = j+1
+    raw["date"] = pd.to_datetime(raw["timestamp"])
+    utc = raw["date"].dt.tz_convert("Etc/GMT+1")
+    raw["Stn.No."] = j
+    raw["Year"] = utc.dt.year
+    raw["Month"] = utc.dt.month
+    raw["Day"] = utc.dt.day
+    raw["HH"] = utc.dt.hour
+    raw["T"] = raw["temp"]
+    raw["W"] = raw["wspd"]
+    raw["WD"] = raw["wdir"]
+    raw["u"] = -raw["W"] * np.cos(2 * np.pi / 360 * (90 - raw["WD"]))
+    raw["v"] = -raw["W"] * np.sin(2 * np.pi / 360 * (90 - raw["WD"]))
+    raw["p"] = raw["pres"]
+    raw["R"] = raw["rr"]
+    raw["RH"] = raw["rh"]
+    raw["is_sound"] = ".false."
+    clean = raw[
+        [
+            "Stn.No.",
+            "Year",
+            "Month",
+            "Day",
+            "HH",
+            "T",
+            "W",
+            "WD",
+            "u",
+            "v",
+            "p",
+            "R",
+            "RH",
+            "is_sound",
+        ]
+    ]
+    cleans = clean.sort_values(by=["Day", "HH"])
+    cleans.to_csv(Path(config["OUT_DIR"]) / f"{i}.csv", index=False)
+    j = j + 1
 
-    for rn in raw['name'].unique():
+    for rn in raw["name"].unique():
         stationlist.append(rn)
 # Get list of stations with data
-st = pd.DataFrame(stationlist,columns=["Station ID"])
-d_station_new = pd.merge(st,df_station,on="Station ID")
+st = pd.DataFrame(stationlist, columns=["Station ID"])
+d_station_new = pd.merge(st, df_station, on="Station ID")
 # Create new csv of station with coordinates for convert.exe
-d_station_new.to_csv(Path(IN_DIR) /f'MO2LITTLER/coordinates_data.csv',index=False)
+d_station_new.to_csv(
+    Path(config["IN_DIR"]) / "MO2LITTLER/coordinates_data.csv", index=False
+)
--- scripts/run_wrfda_pre.sh
+++ scripts/run_wrfda_pre.sh
@@ -7,12 +7,12 @@ echo "  *************************  "
 echo " Preparing assimilation data "
 echo "  *************************  "
 # assimilation window (how many hours before and after the initialization time)
-SDAT=$(date -d "${FCST_YY}-${FCST_MM}-${FCST_DD} ${FCST_ZZ}:00:00 $((${WINDOW_HOUR})) hours ago" +'%Y-%m-%d %H:%M:%S')
+SDAT=$(date -d "${FCST_YY}-${FCST_MM}-${FCST_DD} ${FCST_ZZ}:00:00 $((WINDOW_HOUR)) hours ago" +'%Y-%m-%d %H:%M:%S')
 YYS=${SDAT:0:4}
 MMS=${SDAT:5:2}
 DDS=${SDAT:8:2}
 HHS=${SDAT:11:2}
-EDAT=$(date -d "${FCST_YY}-${FCST_MM}-${FCST_DD} ${FCST_ZZ}:00:00 $((${WINDOW_HOUR})) hours" +'%Y-%m-%d %H:%M:%S')
+EDAT=$(date -d "${FCST_YY}-${FCST_MM}-${FCST_DD} ${FCST_ZZ}:00:00 $((WINDOW_HOUR)) hours" +'%Y-%m-%d %H:%M:%S')
 YYE=${EDAT:0:4}
 MME=${EDAT:5:2}
 DDE=${EDAT:8:2}
@@ -29,31 +29,32 @@ echo " ************************** "
 echo "Running madis_to_little_r..."
 
 #mkdir -p ${MAINDIR}/input/madis_files/MADIS2LITTLER/point
-cd ${SCRIPT_DIR}/MADIS2LITTLER
-rm -rf ${MAINDIR}/input/madis_files/MADIS2LITTLER/point/*
-ln -s ${MADISDIR}/* ${MAINDIR}/input/madis_files/MADIS2LITTLER/point/.
+cd "$SCRIPT_DIR/MADIS2LITTLER" || exit
+rm -rf "$MAINDIR"/input/madis_files/MADIS2LITTLER/point/*
+ln -s "$MADIS_DIR"/* "$MAINDIR"/input/madis_files/MADIS2LITTLER/point/.
 
 # Edit madis_to_little_r.ksh
 rm -f run_madis_to_little_r.ksh
-sed -i "26s~.*~export MADIS_DATA=${MAINDIR}/input/madis_files/MADIS2LITTLER~" run_madis_to_little_r_stat.ksh
-sed -i "27s~.*~export MADIS_STATIC=/home/MODELS/WRFutils/madis-4.3/static/~" run_madis_to_little_r_stat.ksh
-sed -i "37s/.*/export METAR=TRUE/" run_madis_to_little_r_stat.ksh
-sed -i "38s/.*/export MARINE=TRUE/" run_madis_to_little_r_stat.ksh
-sed -i "39s/.*/export GPSPW=FALSE/" run_madis_to_little_r_stat.ksh
-sed -i "40s/.*/export ACARS=TRUE/" run_madis_to_little_r_stat.ksh
-sed -i "41s/.*/export RAOB=TRUE/" run_madis_to_little_r_stat.ksh
-sed -i "42s/.*/export NPN=FALSE/" run_madis_to_little_r_stat.ksh
-sed -i "43s/.*/export MAP=FALSE/" run_madis_to_little_r_stat.ksh
-sed -i "44s/.*/export SATWND=TRUE/" run_madis_to_little_r_stat.ksh
-sed -i "49s/.*/SDATE=$YYS$MMS$DDS$HHS/" run_madis_to_little_r_stat.ksh
-sed -i "50s/.*/EDATE=$YYE$MME$DDE$HHE/" run_madis_to_little_r_stat.ksh
-sed -i "51s/.*/INTERVAL=1/" run_madis_to_little_r_stat.ksh
-sed -i "57s~.*~CODE_DIR=${SCRIPT_DIR}/MADIS2LITTLER/~" run_madis_to_little_r_stat.ksh
+sed -i "5s~.*~export MADIS_DATA=${MAINDIR}/input/madis_files/MADIS2LITTLER~" .env
+sed -i "6s~.*~export MADIS_STATIC=/home/MODELS/WRFutils/madis-4.3/static/~" .env
+sed -i "15s/.*/export METAR=TRUE/" .env
+sed -i "16s/.*/export MARINE=TRUE/" .env
+sed -i "17s/.*/export GPSPW=FALSE/" .env
+sed -i "18s/.*/export ACARS=TRUE/" .env
+sed -i "19s/.*/export RAOB=TRUE/" .env
+sed -i "20s/.*/export NPN=FALSE/" .env
+sed -i "21s/.*/export MAP=FALSE/" .env
+sed -i "22s/.*/export SATWND=TRUE/" .env
+sed -i "26s/.*/export MADIS_SDATE=$YYS$MMS$DDS$HHS/" .env
+sed -i "27s/.*/export MADIS_EDATE=$YYE$MME$DDE$HHE/" .env
+sed -i "28s/.*/export MADIS_INTERVAL=1/" .env
+sed -i "33s~.*~export MADIS_CODE_DIR=${SCRIPT_DIR}/MADIS2LITTLER/~" .env
 ln -s run_madis_to_little_r_stat.ksh run_madis_to_little_r.ksh
 
-
-./run_madis_to_little_r.ksh >& log.madistolittler & tail --pid=$! -f log.madistolittler
-mv log.madistolittler ${MAINDIR}/input/madis_files/MADIS2LITTLER/.
+source ./.env
+./run_madis_to_little_r.ksh >&log.madistolittler &
+tail --pid=$! -f log.madistolittler
+mv log.madistolittler "$MAINDIR"/input/madis_files/MADIS2LITTLER/.
 
 echo "End of madis to little r!"
 echo " *********************** "
@@ -62,39 +63,38 @@ echo " ************************** "
 echo "Converting AWS to little r..."
 
 #mkdir -p ${MAINDIR}/input/aws_files/point
-cd ${SCRIPT_DIR}/MO2LITTLER
-rm -rf ${MAINDIR}/input/aws_files/MO2LITTLER/point/*
-rm ${SCRIPT_DIR}/MO2LITTLER/*.csv
+cd "$SCRIPT_DIR/MO2LITTLER" || exit
+rm -rf "$MAINDIR"/input/aws_files/MO2LITTLER/point/*
+rm "$SCRIPT_DIR"/MO2LITTLER/*.csv
 
 # Edit prepare_stations.py
-sed -i "7s~.*~IN_DIR  = '${AWSDIR}'~" prepare_stations.py
+sed -i "1s~.*~export IN_DIR='${AWS_DIR}'~" .env
 # sed -i "8s~.*~WORK_DIR = '${WRF_MAINDIR}/MO2LITTLER'~" prepare_stations.py
-sed -i "9s~.*~OUT_DIR = '${MAINDIR}/input/aws_files/MO2LITTLER/point'~" prepare_stations.py
-sed -i "10s/.*/yyyymmdd = '${YYA}-${MMA}-${DDA}'/" prepare_stations.py
-sed -i "11s/.*/init = '${HHA}PHT'/" prepare_stations.py
+sed -i "2s~.*~export OUT_DIR='${MAINDIR}/input/aws_files/MO2LITTLER/point'~" .env
+sed -i "3s/.*/export YYYYMMDD='${YYA}-${MMA}-${DDA}'/" .env
+sed -i "4s/.*/export HH_INIT='${HHA}PHT'/" .env
 
 echo " ************************** "
 echo "Preparing station data..."
 $PYTHON prepare_stations.py
 
-ln -s ${MAINDIR}/input/aws_files/MO2LITTLER/point/*.csv .
-ln -s ${MAINDIR}/input/aws_files/MO2LITTLER/coordinates_data.csv .
+ln -s "$MAINDIR"/input/aws_files/MO2LITTLER/point/*.csv .
+ln -s "$MAINDIR"/input/aws_files/MO2LITTLER/coordinates_data.csv .
 
 ./convert.exe
 
-mkdir -p ${MAINDIR}/input/aws_files/MO2LITTLER/little_r_obs/$YYS$MMS$DDS$HHS
-mv obs_MOstns_r* ${MAINDIR}/input/aws_files/MO2LITTLER/little_r_obs/$YYS$MMS$DDS$HHS
+mkdir -p "$MAINDIR/input/aws_files/MO2LITTLER/little_r_obs/$YYS$MMS$DDS$HHS"
+mv obs_MOstns_r* "$MAINDIR/input/aws_files/MO2LITTLER/little_r_obs/$YYS$MMS$DDS$HHS"
 
 echo "End of aws to little r!"
 echo " *********************** "
 
 # Combine converted AWS data per hour
-DIRAWS=${MAINDIR}/input/aws_files/MO2LITTLER/little_r_obs/$YYS$MMS$DDS$HHS
-cd $DIRAWS
+DIRAWS="$MAINDIR/input/aws_files/MO2LITTLER/little_r_obs/$YYS$MMS$DDS$HHS"
+cd "$DIRAWS" || exit
 AWS_LIST=("")
 CDAT=$SDAT
-while [ "$CDAT" != "$TDAT" ]
-do
+while [ "$CDAT" != "$TDAT" ]; do
   YY=${CDAT:0:4}
   MM=${CDAT:5:2}
   DD=${CDAT:8:2}
@@ -104,16 +104,15 @@ do
 done
 
 # Combine converted MADIS data per hour
-cd ${MAINDIR}/input/madis_files/MADIS2LITTLER/little_r_obs
+cd "$MAINDIR/input/madis_files/MADIS2LITTLER/little_r_obs" || exit
 MADIS_LIST=("")
 CDAT=$SDAT
-while [ "$CDAT" != "$TDAT" ]
-do
+while [ "$CDAT" != "$TDAT" ]; do
   YY=${CDAT:0:4}
   MM=${CDAT:5:2}
   DD=${CDAT:8:2}
   HH=${CDAT:11:2}
-  cat $YY$MM$DD$HH/*/* > obs_$YY$MM$DD$HH
+  cat "$YY$MM$DD$HH"/*/* >"obs_$YY$MM$DD$HH"
   MADIS_LIST=(${MADIS_LIST[@]} "obs_$YY$MM$DD$HH")
   CDAT=$(date -d "$CDAT 1 hour" +'%Y-%m-%d %H:%M:%S')
 done
@@ -121,28 +120,27 @@ done
 # Combine MADIS and AWS data per hour
 FILE_LIST=("")
 COUNT=0
-while [ "$COUNT" != 7 ]
-do
-  cat ${MADIS_LIST[${COUNT}]} $DIRAWS/${AWS_LIST[${COUNT}]} > obsall_${COUNT}
+while [ "$COUNT" != 7 ]; do
+  cat ${MADIS_LIST[${COUNT}]} $DIRAWS/${AWS_LIST[${COUNT}]} >obsall_${COUNT}
   FILE_LIST=(${FILE_LIST[@]} "obsall_${COUNT}")
-  COUNT=$(( $COUNT + 1 ))
+  COUNT=$((COUNT + 1))
 done
 
 # Create master assimilation data file (.ascii)
 echo "concatenating : " ${FILE_LIST[*]}
-cat $(echo ${FILE_LIST[*]}) > ob_$YYS$MMS$DDS${HHS}.ascii
+cat $(echo ${FILE_LIST[*]}) >"ob_$YYS$MMS$DDS${HHS}.ascii"
 
 # Cleanup files and remove redundant observation data for assimilation to free storage space
-rm -rf ${MAINDIR}/input/aws_files/MO2LITTLER/little_r_obs/$YYS$MMS$DDS$HHS # MADIS data from individual sources
-rm ${MAINDIR}/input/aws_files/MO2LITTLER/little_r_obs/obs_* # concatenated MADIS data
-rm ${MAINDIR}/input/aws_files/MO2LITTLER/little_r_obs/obsall_* # concatenated MADIS + AWS data
+rm -rf "$MAINDIR/input/aws_files/MO2LITTLER/little_r_obs/$YYS$MMS$DDS$HHS" # MADIS data from individual sources
+rm "$MAINDIR"/input/aws_files/MO2LITTLER/little_r_obs/obs_*                # concatenated MADIS data
+rm "$MAINDIR"/input/aws_files/MO2LITTLER/little_r_obs/obsall_*             # concatenated MADIS + AWS data
 
 # -------------------------------------------- #
 #                 Run OBSPROC                  #
 # -------------------------------------------- #
-cd ${WRF_MAINDIR}/WRF3DVar/OBSPROC
-rm ob_$YYS$MMS$DDS${HHS}.ascii
-ln -s ${MAINDIR}/input/madis_files/MADIS2LITTLER/little_r_obs/ob_$YYS$MMS$DDS${HHS}.ascii .
+cd "$WRF_MAINDIR/WRF3DVar/OBSPROC" || exit
+rm "ob_$YYS$MMS$DDS${HHS}.ascii"
+ln -s "$MAINDIR/input/madis_files/MADIS2LITTLER/little_r_obs/ob_$YYS$MMS$DDS${HHS}.ascii" .
 
 # Edit namelist.obsproc
 rm -f namelist.obsproc
@@ -155,11 +153,12 @@ sed -i "79s/.*/ NUM_SLOTS_PAST  =           ${WINDOW_HOUR},/" namelist.obsproc_3
 sed -i "80s/.*/ NUM_SLOTS_AHEAD =           ${WINDOW_HOUR},/" namelist.obsproc_3DVar
 ln -s namelist.obsproc_3DVar namelist.obsproc
 
-rm -f obs_* UV.txt HEIGHT.txt SPD.txt DIR.txt TEMP.txt RH.txt PRES.txt 
+rm -f obs_* UV.txt HEIGHT.txt SPD.txt DIR.txt TEMP.txt RH.txt PRES.txt
 
 echo " *********************** "
 echo "Running obsproc.exe..."
-./obsproc.exe >& log.obsproc & tail --pid=$! -f log.obsproc
+./obsproc.exe >&log.obsproc &
+tail --pid=$! -f log.obsproc
 echo "End of obsproc.exe!"
 echo " *********************** "
 # -------------------------------------------- #
@@ -169,39 +168,41 @@ echo "  ********************  "
 echo " Running WRFDA "
 echo "  ********************  "
 
-cd ${WRF_MAINDIR}/WRF3DVar
-rm -f fg wrfbdy_d01 wrfinput_d01 ob.ascii 
-ln -s ${WRF_REALDIR}/wrfreal_tmp/wrfinput_d01_${FCST_YY}-${FCST_MM}-${FCST_DD}_${FCST_ZZ} fg
-ln -s ${WRF_REALDIR}/wrfreal_tmp/wrfbdy_d01_${FCST_YY}-${FCST_MM}-${FCST_DD}_${FCST_ZZ} wrfbdy_d01
-ln -s ${WRF_MAINDIR}/WRF3DVar/OBSPROC/obs_gts_${FCST_YY}-${FCST_MM}-${FCST_DD}_${FCST_ZZ}:00:00.3DVAR ob.ascii
+cd "$WRF_MAINDIR/WRF3DVar" || exit
+rm -f fg wrfbdy_d01 wrfinput_d01 ob.ascii
+ln -s "$WRF_REALDIR/wrfreal_tmp/wrfinput_d01_${FCST_YY}-${FCST_MM}-${FCST_DD}_${FCST_ZZ}" fg
+ln -s "$WRF_REALDIR/wrfreal_tmp/wrfbdy_d01_${FCST_YY}-${FCST_MM}-${FCST_DD}_${FCST_ZZ}" wrfbdy_d01
+ln -s "$WRF_MAINDIR/WRF3DVar/OBSPROC/obs_gts_${FCST_YY}-${FCST_MM}-${FCST_DD}_${FCST_ZZ}:00:00.3DVAR" ob.ascii
 
 # Edit namelist.input (WRFDA)
 rm -f namelist.input
-sed -i "45s/.*/analysis_date=\"${FCST_YY}-${FCST_MM}-${FCST_DD}_${FCST_ZZ}:00:00.0000\",/" namelist.input_${NAMELIST_3DVar}
-sed -i "52s/.*/time_window_min=\"${YYS}-${MMS}-${DDS}_${HHS}:00:00.0000\",/" namelist.input_${NAMELIST_3DVar}
-sed -i "55s/.*/time_window_max=\"${YYE}-${MME}-${DDE}_${HHE}:00:00.0000\",/" namelist.input_${NAMELIST_3DVar}
-sed -i "58s/.*/start_year=${FCST_YY},/" namelist.input_${NAMELIST_3DVar}
-sed -i "59s/.*/start_month=${FCST_MM},/" namelist.input_${NAMELIST_3DVar}
-sed -i "60s/.*/start_day=${FCST_DD},/" namelist.input_${NAMELIST_3DVar}
-sed -i "61s/.*/start_hour=${FCST_ZZ},/" namelist.input_${NAMELIST_3DVar}
-sed -i "62s/.*/end_year=${FCST_YY2},/" namelist.input_${NAMELIST_3DVar}
-sed -i "63s/.*/end_month=${FCST_MM2},/" namelist.input_${NAMELIST_3DVar}
-sed -i "64s/.*/end_day=${FCST_DD2},/" namelist.input_${NAMELIST_3DVar}
-sed -i "65s/.*/end_hour=${FCST_ZZ2},/" namelist.input_${NAMELIST_3DVar}
-ln -s namelist.input_${NAMELIST_3DVar} namelist.input
+sed -i "45s/.*/analysis_date=\"${FCST_YY}-${FCST_MM}-${FCST_DD}_${FCST_ZZ}:00:00.0000\",/" "namelist.input_$NAMELIST_3DVar"
+sed -i "52s/.*/time_window_min=\"${YYS}-${MMS}-${DDS}_${HHS}:00:00.0000\",/" "namelist.input_$NAMELIST_3DVar"
+sed -i "55s/.*/time_window_max=\"${YYE}-${MME}-${DDE}_${HHE}:00:00.0000\",/" "namelist.input_$NAMELIST_3DVar"
+sed -i "58s/.*/start_year=${FCST_YY},/" "namelist.input_$NAMELIST_3DVar"
+sed -i "59s/.*/start_month=${FCST_MM},/" "namelist.input_$NAMELIST_3DVar"
+sed -i "60s/.*/start_day=${FCST_DD},/" "namelist.input_$NAMELIST_3DVar"
+sed -i "61s/.*/start_hour=${FCST_ZZ},/" "namelist.input_$NAMELIST_3DVar"
+sed -i "62s/.*/end_year=${FCST_YY2},/" "namelist.input_$NAMELIST_3DVar"
+sed -i "63s/.*/end_month=${FCST_MM2},/" "namelist.input_$NAMELIST_3DVar"
+sed -i "64s/.*/end_day=${FCST_DD2},/" "namelist.input_$NAMELIST_3DVar"
+sed -i "65s/.*/end_hour=${FCST_ZZ2},/" "namelist.input_$NAMELIST_3DVar"
+ln -s "namelist.input_$NAMELIST_3DVar" namelist.input
 
 # Run WRF-3DVar
 echo " *********************** "
 echo "Running da_wrfvar..."
 # ./da_wrfvar.exe >& log.wrfda & tail --pid=$! -f log.wrfda
-srun ./da_wrfvar.exe >& log.wrfda & tail --pid=$! -f rsl.error.0000
+srun ./da_wrfvar.exe >&log.wrfda &
+tail --pid=$! -f rsl.error.0000
 echo "End of da_wrfvar!"
 echo " *********************** "
 
 # Update lateral boundary conditions
 echo " *********************** "
 echo "Running da_update_bc..."
-srun ./da_update_bc.exe >& log.bcupdate & tail --pid=$! -f rsl.error.0000
+srun ./da_update_bc.exe >&log.bcupdate &
+tail --pid=$! -f rsl.error.0000
 echo "End of da_update_bc!"
 echo " *********************** "
 
