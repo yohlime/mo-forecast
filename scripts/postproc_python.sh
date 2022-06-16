04a4f8beaf4c1e55b0556d95883814cc910e7e77
Emilio Gozo <egozo@observatory.ph>
2022-06-16 08:42:15 +0800

    fix: apply SC suggestions

--- scripts/postproc_python.sh
+++ scripts/postproc_python.sh
@@ -8,11 +8,11 @@ echo "---------------------------------"
 #              Postprocessing                  #
 # -------------------------------------------- #
 
-cd ${SCRIPT_DIR}/python
+cd "$SCRIPT_DIR/python" || exit
 WRF_OUT_FILE="wrfout_d01_${FCST_YY}-${FCST_MM}-${FCST_DD}_${FCST_ZZ}_00_00_5-day_fcst_rain"
-${PYTHON} process_wrf.py -i ${WRF_OUT_FILE} -o ${OUTDIR}
+${PYTHON} process_wrf.py -i "$WRF_OUT_FILE" -o "$OUTDIR"
 
 echo "---------------------------------"
 echo " Python postprocessing finished! "
 echo "---------------------------------"
-cd ${MAINDIR}
+cd "$MAINDIR" || exit
