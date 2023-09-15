70dd08cd05dd5037347936c8460e3a31cdcaf344
Emilio Gozo <emiliogozo@proton.me>
2023-09-15 14:17:10 +0800

    fest: do not throw errors

--- scripts/python/config.py
+++ scripts/python/config.py
@@ -7,12 +7,12 @@ from cartopy import crs as ccrs
 from dataclasses import dataclass, field
 
 
-@dataclass
+@dataclass(repr=False)
 class Config:
     tz: pytz.BaseTzInfo = field(init=False, default=pytz.timezone("Asia/Manila"))
     plot_proj: ccrs.PlateCarree = field(init=False, default=ccrs.PlateCarree())
     data_dir: Path = field(init=False)
-    wrf_maindir: Path = field(init=False)
+    wrf_realdir: Path = field(init=False)
     script_dir: Path = field(init=False)
     wrf_forecast_days: int = field(init=False)
     wrf_run_names: list[str] = field(init=False)
@@ -22,20 +22,24 @@ class Config:
     def __post_init__(self):
         data_dir = os.getenv("OUTDIR")
         if data_dir is None:
-            raise NotADirectoryError("OUTDIR not set")
+            self.data_dir = Path("./output")
+            warnings.warn(f"OUTDIR not set, data_dir set to '{self.data_dir}'")
         else:
             self.data_dir = Path(data_dir)
 
-        wrf_maindir = os.getenv("WRF_REALDIR")
-        if wrf_maindir is None:
-            raise NotADirectoryError("WRF_REALDIR not set")
+        wrf_realdir = os.getenv("WRF_REALDIR")
+        if wrf_realdir is None:
+            self.wrf_realdir = Path("./model/WRF")
+            warnings.warn(
+                f"WRF_REALDIR not set, wrf_realdir set to '{self.wrf_realdir}'"
+            )
         else:
-            self.wrf_maindir = Path(wrf_maindir)
+            self.wrf_realdir = Path(wrf_realdir)
 
         script_dir = os.getenv("SCRIPT_DIR")
         if script_dir is None:
-            self.script_dir = self.wrf_maindir / "scripts"
-            warnings.warn(f"SCRIPT_DIR not set, script_dir set to '{script_dir}'")
+            self.script_dir = Path("./scripts")
+            warnings.warn(f"SCRIPT_DIR not set, script_dir set to '{self.script_dir}'")
         else:
             self.script_dir = Path(script_dir)
 
@@ -43,20 +47,21 @@ class Config:
         if wrf_forecast_days is None:
             self.wrf_forecast_days = 3
             warnings.warn(
-                f"WRF_FCST_DAYS not set, wrf_forecast_days set to '{wrf_forecast_days}'"
+                f"WRF_FCST_DAYS not set, wrf_forecast_days set to '{self.wrf_forecast_days}'"
             )
         else:
             self.wrf_forecast_days = int(wrf_forecast_days)
 
         wrf_run_names = os.getenv("WRF_RUN_NAMES")
         if wrf_run_names is None:
-            wrf_run_names = "run1:run2"
-            warnings.warn(
-                f"WRF_RUN_NAMES not set, wrf_run_names set to '{wrf_run_names}'"
-            )
+            wrf_run_names = "run1"
         self.wrf_run_names = wrf_run_names.split(":")
+        if wrf_run_names is None:
+            warnings.warn(
+                f"WRF_RUN_NAMES not set, wrf_run_names set to '{[self.wrf_run_names]}'"
+            )
         self.wrf_dirs = [
-            self.wrf_maindir / wrf_run_name for wrf_run_name in self.wrf_run_names
+            self.wrf_realdir / wrf_run_name for wrf_run_name in self.wrf_run_names
         ]
 
         self.num_ens_member = len(self.wrf_run_names)
--- tests/python/test_config.py
+++ tests/python/test_config.py
@@ -1,18 +1,31 @@
+import pytest
 from config import Config
 
 
+def test_config_default():
+    with pytest.warns(UserWarning):
+        conf = Config()
+        assert str(conf.tz) == "Asia/Manila"
+        assert str(conf.data_dir) == "output"
+        assert str(conf.wrf_realdir) == "model/WRF"
+        assert str(conf.script_dir) == "scripts"
+        assert conf.wrf_forecast_days == 3
+        for i, wrf_dir in enumerate(conf.wrf_dirs):
+            assert wrf_dir.name == f"run{i+1}"
+
+
 def test_config(monkeypatch):
-    monkeypatch.setenv("OUTDIR", "output")
-    monkeypatch.setenv("WRF_REALDIR", "forecast")
+    monkeypatch.setenv("OUTDIR", "forecast/output")
+    monkeypatch.setenv("WRF_REALDIR", "forecast/model/WRF")
     monkeypatch.setenv("SCRIPT_DIR", "forecast/scripts")
-    monkeypatch.setenv("WRF_FCST_DAYS", "3")
+    monkeypatch.setenv("WRF_FCST_DAYS", "5")
     monkeypatch.setenv("WRF_RUN_NAMES", "run1:run2:run3")
 
     conf = Config()
     assert str(conf.tz) == "Asia/Manila"
-    assert str(conf.data_dir) == "output"
-    assert str(conf.wrf_maindir) == "forecast"
+    assert str(conf.data_dir) == "forecast/output"
+    assert str(conf.wrf_realdir) == "forecast/model/WRF"
     assert str(conf.script_dir) == "forecast/scripts"
-    assert conf.wrf_forecast_days == 3
+    assert conf.wrf_forecast_days == 5
     for i, wrf_dir in enumerate(conf.wrf_dirs):
         assert wrf_dir.name == f"run{i+1}"
